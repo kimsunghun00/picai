f8e0bcb25a6bea9f996141d26a574b73d6a73328
KIMSUNGHUN <61981989+kimsunghun00@users.noreply.github.com>
2022-08-05 14:37:45 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     setup(
         name='sunghun_lib',
         version='0.0.1',
-        desrciption='sunghun pip install test'
-        url='https://github.com/kimsunghun00/picai.git'
-        author='sunghun'
+        desrciption='sunghun pip install test',
+        url='https://github.com/kimsunghun00/picai.git',
+        author='sunghun',
         author_meail='uiop8533@gmail.com',
         python_requires='>=3.7',
         install_requires=requirements
